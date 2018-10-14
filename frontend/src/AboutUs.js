f6c3214c9cffbd93c98bab44ae3d0794f7538d43
sidmadala <22417141+sidmadala@users.noreply.github.com>
2018-10-13 21:37:36 -0400

    logo update/aboutus updates

--- frontend/src/AboutUs.js
+++ frontend/src/AboutUs.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
-class App extends Component {
+class AboutUs extends Component {
   render() {
     return (
       <div>
+                
+        
+        
+        
         Oxfam is a global organization working to end the injustice of poverty. 
         We help people build better futures for themselves, hold the powerful accountable, and save lives in disasters. 
         Our mission is to tackle the root causes of poverty and create lasting solutions.
@@ -12,4 +16,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default AboutUs;
