de5571dfdf996c081aea0aa14cf9396781b9295c
eddyihminen <elindy26@gmail.com>
2018-10-13 20:28:01 -0400

    Added Oxfam logo

--- frontend/src/NavBar.js
+++ frontend/src/NavBar.js
@@ -5,11 +5,11 @@ class NavBar extends Component {
     return (
         <nav className="uk-navbar-container" uk-navbar>
             <div className="uk-navbar-left">
-                <ul className="uk-navbar-nav"> 
+                <ul className="uk-navbar-nav">
+                    <a class="uk-navbar-item uk-logo" href="">Oxfam</a>
                     <li className = {window.location.href === 'http://localhost:3000/' ? "uk-active" : "uk-parent"}><a href="/">Home</a></li>
                     <li className = {window.location.href === 'http://localhost:3000/candidates' ? "uk-active" : "uk-parent"}><a href="/candidates">Candidates</a></li>
                     <li className = {window.location.href === 'http://localhost:3000/about' ? "uk-active" : "uk-parent"}><a href="/about">About Us</a></li>
-                    <li><a href=""></a></li>
                 </ul>
             </div>
         </nav>
